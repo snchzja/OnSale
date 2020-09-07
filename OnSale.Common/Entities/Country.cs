2a79241092e57414d1c98ca256eefae7de7859b9
Jose Sanchez <snchzja@gmail.com>
2020-09-07 16:27:36 -0300

    Improve countries CRUD

---
+++ OnSale.Common/Entities/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OnSale.Common.Entities
+{
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+    }
+}
